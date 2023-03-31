287623327fc1ccb7b6dd2f467fdfc85545a35851
Programmer-SXB <129528050+Programmer-SXB@users.noreply.github.com>
2023-03-31 19:44:33 +0500

    Create Dump.py

---
+++ Dump.py
@@ -0,0 +1,9 @@
+import os,platform
+os.system('git pull')
+os.systeem('clear')
+print(' SXB Tools Loading')
+xd=platform.architecture()[0]
+if xd=="32bit":
+    print('[+] \33[1;32mTHIS IS ONLY 64 BIT')
+if xd=="64bit":
+    __import__("Dump")
